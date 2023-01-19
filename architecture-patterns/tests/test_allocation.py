f9274ee3bf93eafc69169275e1b9ce8b4a826edc
Hamid Adesokan <aadehamid@gmail.com>
2023-01-19 14:44:58 -0600

    created a test file for allocation class

---
+++ architecture-patterns/tests/test_allocation.py
@@ -0,0 +1,19 @@
+
+class AllocationTests:
+    def test_reduce_qty(self):
+        pass
+
+    def test_batch_quantity_more_than_order_line(self):
+        pass
+
+    def test_cannot_repeat_allocation(self):
+        pass
+
+    def test_allocate_warehouse_first(self):
+        pass
+
+    def test_eta_present(self):
+        pass
+
+    def test_allocate_shipment_earliest_eta(self):
+        pass

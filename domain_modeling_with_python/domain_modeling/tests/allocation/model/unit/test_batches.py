00a9ea15b7d941ebcb3c17b8f3cd9147de97494a
Hamid Adesokan <aadehamid@gmail.com>
2023-01-21 19:50:49 -0600

    updated test suite for batches

--- domain_modeling_with_python/domain_modeling/tests/allocation/model/unit/test_batches.py
+++ domain_modeling_with_python/domain_modeling/tests/allocation/model/unit/test_batches.py
@@ -5,7 +5,8 @@ from model import OrderLine, Batch
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
-class AllocationTests:
+
+class BatchTests:
 
     def make_batch_and_line(self, sku, batch_qty, line_qty):
         return (Batch("batch-001", sku, batch_qty, eta=today),
@@ -24,19 +25,32 @@ class AllocationTests:
         assert large_batch.can_allocate(small_line)
 
     def test_cannot_allocate_if_available_less_than_required_order_line(self):
-        pytest.fail("todo")
+        small_batch, large_line = self.make_batch_and_line("ELEGANT-LAMP", 2, 20)
+        assert small_batch.can_allocate(large_line) is False
 
     def test_can_allocate_if_available_equal_required_order_line(self):
-        pytest.fail("todo")
+        batch, line = self.make_batch_and_line("ELEGANT-LAMP", 2, 2)
+        assert batch.can_allocate(line)
 
-    def test_cannot_repeat_allocation(self):
-        pytest.fail("todo")
+    def test_cannot_allocate_if_skus_do_not_match(self):
+        batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+        different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+        assert batch.can_allocate(different_sku_line) is False
 
-    def test_prefers_warehouse_batches_to_shipment(self):
-        pytest.fail("todo")
 
-    def test_eta_present_in_current_shipping_batches(self):
-        pytest.fail("todo")
+    def test_allocation_is_idempotent(self):
+        batch, unallocated_line = self.make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+        batch.allocate(unallocated_line)
+        batch.allocate(unallocated_line)
+        assert batch.available_quantity == 18
 
-    def test_prefer_earlier_batches_of_shipment(self):
-        pytest.fail("todo")
+    def test_can_only_deallocate_allocated_lines(self):
+        batch, unallocated_line = self.make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+        batch.deallocate(unallocated_line)
+        assert batch.available_quantity == 20
+
+    def test_deallocate(self):
+        batch, line = self.make_batch_and_line("EXPENSIVE-FOOTSTOOL", 20, 2)
+        batch.allocate(line)
+        batch.deallocate(line)
+        assert batch.available_quantity == 20

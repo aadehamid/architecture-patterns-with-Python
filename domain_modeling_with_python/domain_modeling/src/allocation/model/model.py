fdff3b538b406cc0481dea86faeaa56098938de7
Hamid Adesokan <aadehamid@gmail.com>
2023-01-21 19:49:47 -0600

    updated model module with allocate function and OutOfStock class

--- domain_modeling_with_python/domain_modeling/src/allocation/model/model.py
+++ domain_modeling_with_python/domain_modeling/src/allocation/model/model.py
@@ -1,25 +1,72 @@
 #%%
-from typing import Optional
+from typing import Optional, NewType, List, Set
 from dataclasses import dataclass
 from datetime import date
 
 # %%
-@dataclass
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
+@dataclass(frozen=True)
 class OrderLine:
     orderid: str
     sku: str
     qty: int
 
 class Batch:
-    def __init__(self, ref: str, sku: str,
-                 qty: int, eta: Optional[date]):
+    def __init__(self, ref: Reference, sku: Sku,
+                 qty: Quantity, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()
+        # self.available_quantity = qty
 
     def allocate(self, line: OrderLine) -> None:
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+            # self.available_quantity -= line.qty
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+            # self.available_quantity += line.qty
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def __eq__(self, other: object):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+class OutOfStock(Exception):
+    pass
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
